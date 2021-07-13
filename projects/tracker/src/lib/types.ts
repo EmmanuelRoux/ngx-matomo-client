b1353b04773a52399606a9b75358ef210e6ee61a
Emmanuel Roux <1926413+EmmanuelRoux@users.noreply.github.com>
2021-07-13 21:06:09 +0200

    docs(tracker): fix typo
    closes #10

--- projects/tracker/src/lib/types.ts
+++ projects/tracker/src/lib/types.ts
@@ -4,7 +4,7 @@ export type Getters<T, U = any> = keyof T &
     [P in keyof T]: T[P] extends () => U ? P : never;
   }[keyof T];
 
-/** Extract from a type T all getter-like method keys */
+/** Extract all methods from a type T */
 export type Methods<T> = keyof T &
   {
     [P in keyof T]: T[P] extends (...args: any[]) => any ? P : never;
