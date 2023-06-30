5df6bb9d1969dacb5991fcb6550d4d02c62cdfef
Emmanuel Roux <1926413+EmmanuelRoux@users.noreply.github.com>
2023-06-30 18:16:41 +0200

    chore(schematics): remove dependencies on non-public @schematics/angular api

---
+++ projects/ngx-matomo-client/schematics/utils/index.ts
@@ -0,0 +1,7 @@
+export * from './change';
+export * from './ast-utils';
+export * from './json-file';
+export * from './workspace';
+export * from './find-module';
+export * from './dependencies';
+export * from './workspace-models';
