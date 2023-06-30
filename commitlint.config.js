229614da009520691e8fa0afee8ad48c9e54dfab
Emmanuel Roux <1926413+EmmanuelRoux@users.noreply.github.com>
2023-06-30 22:58:46 +0200

    chore: update commitlint config to allow longer messages

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,8 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [2, 'always', 200],
+    'footer-max-line-length': [2, 'always', 200],
+    'header-max-length': [2, 'always', 200],
+  },
+};
