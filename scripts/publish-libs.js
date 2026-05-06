4071484c4f245b54c9766f880b8c1cc0fae6018c
Emmanuel Roux <1926413+EmmanuelRoux@users.noreply.github.com>
2026-05-06 21:30:19 +0200

    chore: upgrade dependencies

--- scripts/publish-libs.js
+++ scripts/publish-libs.js
@@ -8,15 +8,8 @@ const { checkNpmToken, readPkgJson, getChannel, getRegistry } = require('./utils
 
 const [version, channel] = process.argv.slice(2);
 
-function setUpNpmAuth(dir, registry) {
-  const npmrc = path.resolve(dir, '.npmrc');
-
-  checkNpmToken();
-  fs.writeFileSync(npmrc, nerfDart(registry) + ':_authToken=${NPM_TOKEN}');
-}
-
-function getPublishCmd(npmrc, distTag, registry) {
-  return `npm publish --userconfig ${npmrc} --tag ${distTag} --registry ${registry}`;
+function getPublishCmd(distTag, registry) {
+  return `npm publish --tag ${distTag} --registry ${registry}`;
 }
 
 function publishCallback(error) {
@@ -27,17 +20,15 @@ function publishCallback(error) {
 }
 
 const baseDir = path.resolve(DIST_DIR, LIB_DIR_NAME);
-const npmrc = path.resolve(baseDir, '.npmrc');
 const pkg = readPkgJson(baseDir);
 const registry = getRegistry(baseDir);
 const distTag = getChannel(channel);
-const publishCmd = getPublishCmd(npmrc, distTag, registry);
+const publishCmd = getPublishCmd(distTag, registry);
 
 console.log(
   `Publishing version ${version} of ${LIB_DIR_NAME} to npm registry on dist-tag ${distTag}`,
 );
 
-setUpNpmAuth(baseDir, registry);
 exec(publishCmd, { cwd: baseDir }, publishCallback);
 
 console.log(`Published ${pkg.name}@${version} to dist-tag @${distTag} on ${registry}`);
