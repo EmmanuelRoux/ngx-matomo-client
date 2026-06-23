df6d3e3c4f29294d3386cd5f31cf30f731fbec89
Mohamed Ben Makhlouf <med.b.makhlouf@gmail.com>
2026-06-23 20:08:39 +0200

    refactor: simplify TrackFormFieldDirective by removing unnecessary effects and using afterRenderEffect for improved clarity

--- projects/ngx-matomo-client/form-analytics/directives/track-form-field.directive.ts
+++ projects/ngx-matomo-client/form-analytics/directives/track-form-field.directive.ts
@@ -1,55 +1,28 @@
-import {
-  AfterViewInit,
-  booleanAttribute,
-  Directive,
-  effect,
-  ElementRef,
-  inject,
-  input,
-} from '@angular/core';
+import { afterRenderEffect, booleanAttribute, Directive, inject, input } from '@angular/core';
 import { throwFormNotFoundError } from './errors';
 import { TrackFormDirective } from './track-form.directive';
 
 @Directive({
   selector: '[matomoTrackFormField]',
   exportAs: 'matomoTrackFormField',
+  host: {
+    '[attr.data-matomo-ignore]': 'matomoIgnore() ? "" : null',
+    '[attr.data-matomo-name]': 'matomoTrackFormField() || null',
+  },
 })
-export class TrackFormFieldDirective implements AfterViewInit {
-  private readonly elementRef: ElementRef<Element> = inject(ElementRef);
+export class TrackFormFieldDirective {
   private readonly form =
     inject(TrackFormDirective, { optional: true }) ??
     throwFormNotFoundError('[matomoTrackFormField]');
 
-  private initialized = false;
   readonly matomoIgnore = input<boolean>(undefined, { transform: booleanAttribute });
   readonly matomoTrackFormField = input<string | null>();
 
   constructor() {
-    effect(() => {
-      const ignore = this.matomoIgnore();
-      if (ignore) {
-        this.elementRef.nativeElement.setAttribute('data-matomo-ignore', '');
-      } else {
-        this.elementRef.nativeElement.removeAttribute('data-matomo-ignore');
-      }
+    afterRenderEffect(() => {
+      this.matomoTrackFormField();
+      this.track();
     });
-    effect(() => {
-      const name = this.matomoTrackFormField();
-      if (name) {
-        this.elementRef.nativeElement.setAttribute('data-matomo-name', name);
-      } else {
-        this.elementRef.nativeElement.removeAttribute('data-matomo-name');
-      }
-
-      if (this.initialized) {
-        this.track();
-      }
-    });
-  }
-
-  ngAfterViewInit(): void {
-    this.track();
-    this.initialized = true;
   }
 
   track(): void {
